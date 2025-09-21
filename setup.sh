0c6e42617b49b817976c844dceacb6a61f48f7d2
arlomu <arlomudrack@gmail.com>
2025-09-21 15:50:14 +0200

    new

--- setup.sh
+++ setup.sh
@@ -22,11 +22,9 @@ cd ssl
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
 cd ..
 cd ..
-progress "Ensuring systemd is installed..."
-sudo apt install -y systemd >>"$LOG_FILE" 2>&1
 progress "Installing Ollama..."
 curl -fsSL https://ollama.com/install.sh | sh >>"$LOG_FILE" 2>&1
-progress "Pulling Gemma3 model..."
+progress "Pulling Gemma3 1B model..."
 ollama pull gemma3:1b >>"$LOG_FILE" 2>&1
 progress "Installing Node.js and npm..."
 sudo apt install -y nodejs npm >>"$LOG_FILE" 2>&1
@@ -34,12 +32,21 @@ sudo apt install -y nodejs npm >>"$LOG_FILE" 2>&1
 cd Chat
 progress "Installing Node.js dependencies..."
 npm install >>"$LOG_FILE" 2>&1
-
 cd ..
+
 cd Web-search
 progress "Installing Node.js dependencies..."
 npm install >>"$LOG_FILE" 2>&1
+cd ..
 
+cd Deep-search
+progress "Installing Node.js dependencies..."
+npm install >>"$LOG_FILE" 2>&1
+cd ..
+
+cd Code-interpreter
+progress "Installing Node.js dependencies..."
+npm install >>"$LOG_FILE" 2>&1
 cd ..
 
 progress "Setup complete!"
